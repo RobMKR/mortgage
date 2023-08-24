0a36ee7b563722156100cd23407d2877d9d6d9dd
RobMkr <mkrtchyanrobert@gmail.com>
2023-08-24 18:12:58 +0400

    Cleanup

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,23 +11,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    /**
-     * @param array $errors
-     * @return JsonResponse
-     */
-    protected function buildFailedValidationResponse(array $errors): JsonResponse
-    {
-        dd(1);
-        $_errors = [];
-
-        foreach ($errors as $fieldName => $messages){
-            $_errors[] = [
-                'fieldName' => $fieldName,
-                'messages' => $messages
-            ];
-        }
-
-        return ResponseService::invalid($_errors);
-    }
 }
